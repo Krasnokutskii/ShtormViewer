656262991fed3855c5d8e3a01cac0aebdece8ae0
Iaroslav Krasnokutskii <Yroslavk1995@gmail.com>
2021-06-01 21:55:11 +0300

    Added UIActivityViewController info plist

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -24,6 +24,8 @@ class DetailViewController: UIViewController {
         title = "Image \(imageIndex) of \(picturesNumber) "
         navigationItem.largeTitleDisplayMode = .never
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage{
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -40,6 +42,16 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
-
+    @objc func shareTapped(){
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else{
+            print("no image found")
+            return
+        }
+        
+         let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        
+        present(vc, animated: true)
+    }
     
 }
